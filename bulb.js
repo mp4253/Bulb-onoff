a6ea19a5c571ed0fa5ab0eba4cf7608a2d01dc49
mp4253 <111766615+mp4253@users.noreply.github.com>
2023-08-29 10:45:57 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,7 @@
+function turnOnBulb() {
+    document.getElementById('bulbImage').src = 'https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg';
+}
+
+function turnOffBulb() {
+    document.getElementById('bulbImage').src = 'https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg';
+}
